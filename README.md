0297bd3289ec6287e7569cf5d83c88ac534b6e04
balintkaszas <bkaszas@ethz.ch>
2024-07-22 17:53:31 +0200

    Readme and setup

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# DataDrivenLinearization
+# Data-Driven Linearization
+
+A package for data-driven model reduction using linearization techniques.
+
+## Features
+
+## Installation
+
+```sh
+pip install -e . 
+
+## Usage
+
+from data_driven_linearization import DataDrivenLinearization
+
+# Example usage
+model = DataDrivenLinearization(dimension=3, degree=3)
+# ...
+
+
+
+
+
--- setup.py
+++ setup.py
@@ -1,3 +1,34 @@
 from setuptools import setup, find_packages
 
-setup(name='datadrivenlinearization', version='1.0', packages=find_packages())
+# Read the content of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="data_driven_linearization",
+    version="1.0.0",
+    description="A dara-driven model reduction tool using linearization techniques.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Bálint Kaszás",
+    author_email="bkaszas@ethz.ch",
+    url="https://github.com/haller-group/data_driven_linearization",  # Update with your repository URL
+    packages=find_packages(),
+    install_requires=[
+        "numpy>=1.18.0",
+        "scipy>=1.4.0",
+        "scikit-learn>=0.22.0",
+        "torch>=1.4.0",
+        "sympy>=1.5.1",
+        "matplotlib>=3.1.0",
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=5.3.5",
+            "flake8>=3.7.9",
+            "black>=19.10b0",
+        ],
+    },
+    python_requires='>=3.6',
+)
